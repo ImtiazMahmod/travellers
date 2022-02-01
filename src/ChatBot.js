090e159aa75730325de3f1caad2ce0dd5b9c4574
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-02-01 16:46:40 +0600

    chatbot

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -7,12 +7,12 @@ const ChatBot = ({ show, setShow }) => {
       <Modal show={show} dialogClassName="chatBot">
         <Modal.Header closeButton onHide={() => setShow(false)}>
           <Modal.Title>
-            <h3 className="primaryColor fw-bold"> NSTUNow</h3>
+            <h3 className="primaryColor fw-bold"> Traveller</h3>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <iframe
-            title="NSTUNow"
+            title="Traveller"
             src="https://webchat.botframework.com/embed/nstunow?s=1yKYFyasbfM.yXJpJ2i7Nmv3DDHxHqg1r7-Dwjxe1EjdIeX_sI9ZJlY"
             style={{ minWidth: " 100px", width: "100%", minHeight: "300px" }}
           ></iframe>
